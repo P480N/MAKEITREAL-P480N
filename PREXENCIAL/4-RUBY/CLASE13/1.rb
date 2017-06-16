2fc2d18d0ef37cd325e7d04b233653c0538995f8
p480n <p480nzt@gmail.com>
2017-06-16 18:40:34 -0500

    2

---
+++ PREXENCIAL/4-RUBY/CLASE13/1.rb
@@ -0,0 +1,11 @@
+name1 = "ESTIVEN PABON"
+
+ladocuadrado = 5
+
+puts
+puts "welcome #{name1}"
+puts
+puts "El perimetro de un cuadrado de lado #{ladocuadrado} es #{ladocuadrado*4}"
+puts
+puts "El area de un cuadrado de lado #{ladocuadrado} es #{ladocuadrado}"
+puts
