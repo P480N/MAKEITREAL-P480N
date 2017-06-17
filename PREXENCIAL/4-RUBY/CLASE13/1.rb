0f00678df51bdec1508117f7ad1f4b5a3bbba467
p480n <p480nzt@gmail.com>
2017-06-16 20:16:07 -0500

    2

--- PREXENCIAL/4-RUBY/CLASE13/1.rb
+++ PREXENCIAL/4-RUBY/CLASE13/1.rb
@@ -1,11 +1,124 @@
+
+
+#--------------------EJERCICIO 1
+=begin
 name1 = "ESTIVEN PABON"
-
-ladocuadrado = 5
-
 puts
 puts "welcome #{name1}"
-puts
+=end
+
+#--------------------EJERCICIO 2
+=begin
+puts # ladocuadrado = 500
 puts "El perimetro de un cuadrado de lado #{ladocuadrado} es #{ladocuadrado*4}"
 puts
 puts "El area de un cuadrado de lado #{ladocuadrado} es #{ladocuadrado}"
 puts
+=end
+
+#--------------------EJERCICIO 3
+=begin
+print "Ingresa tu nombre: "
+name = gets.chomp
+puts "Hola #{name}"
+=end
+
+#--------------------EJERCICIO 4
+=begin
+print "Ingresa el lado de un cuadrado para calcular el perimetro: "
+ladocuadrado = gets.chomp.to_i
+puts "EL PERIMETRO DE ESE CUADRADO DE LADO #{ladocuadrado} es #{ladocuadrado*4}"
+print "Ingresa el lado de un cuadrado para calcular el area: "
+ladocuadrado = gets.chomp.to_i
+puts "EL AREA DE ESE CUADRADO DE LADO #{ladocuadrado} es #{ladocuadrado*ladocuadrado}"
+=end
+
+#--------------------EJERCICIO 5
+
+# 1- El siguiente código no está imprimiendo el texto esperado, que debería ser "Hola Germán". ¿Cuál es el problema y cómo lo podrías solucionar?
+# name = "Germán"
+# puts "Hola name"
+# 2- ¿Cuál es la utilidad de las variables?
+# Que los datos introducidos en ellas pueden cambiar, se pueden almacenar, escribir o sobreescribir y se pueden utilizar dentro de algoritmos
+# 3- ¿Cuál es la diferencia entre igual (=) y doble igual (==).
+# = iguala dos elementos o da valor a algo y == compara dos elementos
+# 4- ¿Cuál es el problema con el siguiente código?
+# 5park = "Parque 5"
+# el nombre de la variable tiene un numero
+# 5- ¿Cómo podemos pedirle información al usuario desde un programa de Ruby?
+# con el comando gets.chomp
+# 6- ¿Cómo podemos pedirle información al usuario desde un programa de Ruby y convertirlo en número?
+# con gets.chomp.to_i
+# 7- ¿Dónde y cuánto vive una variable?
+# vive en la RAM y su vida depende de que el dispositivo esté encendido, o actualice el navegado
+
+
+#--------------------EJERCICIO 6
+=begin
+
+rescue ExceptionName
+
+end
+print "Ingresá tu peso:"
+peso = gets.chomp.to_i
+print "Ingresá tu altura:"
+altura = gets.chomp.to_f
+imc = peso / (altura**2)
+imc = imc.round (3) #LAS VARIABLES VAN TOMANDO OTRO VALOR CADA QUE SE LE MODIFICA EN UN SIGUIETE RENGLON, EL 78 ACABÓ DE TOMAR LOS VALORES QUE TENIA 77 Y LO REDONDEÓ TRES DECIMALES LUEGO DE LA COMA
+puts "Ome tu INDICE DE MASA CORPORAL es #{imc}"
+puts
+
+print "Ingresá tu edad ome!: "
+edad = gets.chomp.to_i
+puts "Ome dentro de 5 años usted va tener #{edad+5}, vas a estaer viejo mij@"
+puts
+
+actual= 2017
+print "Ingresá tu año de nacimiento ome!: "
+año = gets.chomp.to_i
+puts "Ome vos tenés #{actual-año}"
+puts
+
+print "Ingresá cualquier numero, EL QUE QUIERA!!!: "
+numerito = gets.chomp.to_i
+puts "Ome la mitad de #{numerito} es #{numerito/2}"
+puts
+=end
+
+#--------------------EJERCICIO 6
+=begin
+if true
+  puts "HOLII!"
+end
+
+
+if 1==1
+  puts "son igualitos"
+end
+
+if 1==2
+  puts "son igualitos"
+end
+
+if 1<6
+  puts "es menor"
+end
+
+
+if 3==6
+  puts "son diferentes, ojo"
+end
+=end
+#--------------------EJERCICIO 6
+
+print "ingresa algún numerito: "
+
+num = gets.chomp.to_i
+
+if num < 20
+  puts "ese numero ingresado es menor a 20"
+end
+
+if num > 20
+  puts "ese numero ingresado es mayor a 20"
+end
