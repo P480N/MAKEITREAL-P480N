3414baf12181c8ce13bf0f9a68a7a4d20b4ee3ee
p480n <p480nzt@gmail.com>
2017-06-23 16:06:48 -0500

    CLASE16

---
+++ 2-PRESXSENCIAL/4-RUBY/CLASE16/CLASE16.rb
@@ -0,0 +1,43 @@
+
+
+#------------------#EJERCICIO 1
+
+# print "ingresa los numero de personas: "
+# numer = gets.chomp.to_i
+# personas = []
+# numer.times do
+#   puts "ingresa un nombre: "
+#   personas << gets.chomp.to_s
+# end
+# puts "una de las tantas personas es: #{personas.sample}"
+
+#------------------#EJERCICIO 2
+
+
+# arr=["Hola", "Mundo", 3, "veces"]
+#
+# arr.push("promoción")
+# arr.push("de cositas")
+# arr << "eso si, "
+# arr << "la meta la pones tu"
+# arr.delete_at(1)
+#
+# arr.each_with_index do |element, index|
+#   puts "El valor en la posición #{index} es #{element}"
+# end
+#
+# arr.shuffle!
+#
+# arr.each_with_index do |element, index|
+#   puts "El valor en la posición #{index} es #{element}"
+# end
+
+#------------------#EJERCICIO 3 PARES IMPARES
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# # tu código de la solucion aqui
+# puts "los numeros totales: #{numbers}"
+# par = numbers.select {|x| x.even?}
+# impar = numbers.select {|x| x.odd?}
+# puts "estos son los pares #{par}"
+# puts "estos son los impares #{impar}"
